99f0248e6fd03a7638bec44f43268b78ef49c28a
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-27 12:06:15 +0300

    add

---
+++ 001.BasicSyntaxStatementsLoopsLab/008.DivisibleByThree/DivisibleByThree.cs
@@ -0,0 +1,14 @@
+﻿//Basic syntax, conditional statements and loops - lab, Task 008
+//23.03.2023, 08:34
+using System;
+
+public class DivisibleByThree
+{
+    static void Main()
+    {
+        for(int i = 3; i <= 100; i += 3)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
