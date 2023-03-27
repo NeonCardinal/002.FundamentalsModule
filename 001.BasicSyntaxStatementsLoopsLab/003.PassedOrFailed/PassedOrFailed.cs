58549989e9c74953f2d23b4ba2b19b16a53eb2b1
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-27 12:04:46 +0300

    add

---
+++ 001.BasicSyntaxStatementsLoopsLab/003.PassedOrFailed/PassedOrFailed.cs
@@ -0,0 +1,20 @@
+﻿//Basic syntax, conditional statements and loops - lab, Task 003
+//22.03.2023, 16:53
+using System;
+
+public class PassedOrFailed
+{
+    static void Main()
+    {
+        double grade = double.Parse(Console.ReadLine());
+
+        if(grade >= 3.00)
+        {
+            Console.WriteLine("Passed!");
+        }
+        else
+        {
+            Console.WriteLine("Failed!");
+        }
+    }
+}
