c108cd3b8d8e0c1fa1c9a619c2dc4870c07a1abb
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-28 20:47:41 +0300

    add

---
+++ 002.BasicSyntaxStatementsLoopsExercise/004.PrintAndSum/PrintAndSum.cs
@@ -0,0 +1,23 @@
+﻿//Basic syntax, conditional statements and loops - exercise, Task 004
+//25.03.2023, 10:25
+using System;
+
+public class PrintAndSum
+{
+    static void Main()
+    {
+        int start = int.Parse(Console.ReadLine());
+        int end = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+
+        for(int i = start; i <= end; i++)
+        {
+            Console.Write($"{i} ");
+            sum += i;
+        }
+
+        Console.WriteLine();
+        Console.WriteLine($"Sum: {sum}");
+    }
+}
