5733ef3f70fc5b969e5165de7f0016ddde4d4297
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-28 20:49:40 +0300

    add

---
+++ 002.BasicSyntaxStatementsLoopsExercise/011.Orders/Orders.cs
@@ -0,0 +1,26 @@
+﻿//Basic syntax, conditional statemens and loops - exercise, Task 011
+//25.03.2023, 11:17
+using System;
+
+public class Orders
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        double total = 0.0;
+
+        for (int i = 0; i < n; i++)
+        {
+            double capsulePrice = double.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+            int capsulesCount = int.Parse(Console.ReadLine());
+
+            double sum = (days * capsulesCount) * capsulePrice;
+            total += sum;
+
+            Console.WriteLine($"The price for the coffee is: ${sum:F2}");
+        }
+
+        Console.WriteLine($"Total: ${total:F2}");
+    }
+}
