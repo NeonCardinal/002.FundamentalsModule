f8627da4e4322c17b0372c0925361e25a2f1ae03
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-29 00:00:49 +0300

    add

---
+++ 003.BasicSyntaxStatementsLoopsMoreExercise/001.SortNumbers/SortNumbers.cs
@@ -0,0 +1,26 @@
+﻿//Basic syntax, conditional statements and loops - more exercise, Task 001
+//25.03.2023, 11:27
+using System;
+
+public class SortNumbers
+{
+    static void Main()
+    {
+        int first = int.Parse(Console.ReadLine());
+        int second = int.Parse(Console.ReadLine());
+        int third = int.Parse(Console.ReadLine());
+
+        int[] arr = new int[3];
+
+        arr[0] = first;
+        arr[1] = second;
+        arr[2] = third;
+        Array.Sort(arr);
+        Array.Reverse(arr);
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.WriteLine(arr[i]);
+        }
+    }
+}
