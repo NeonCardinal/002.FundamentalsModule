b11d7e9a088529e97e640bc56e9763c215a4637a
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-29 00:01:46 +0300

    add

---
+++ 003.BasicSyntaxStatementsLoopsMoreExercise/004.ReverseString/ReverseString.cs
@@ -0,0 +1,20 @@
+﻿//Basic syntax, conditional statements and loops - more exercise, Task 004
+//25.03.2023, 11:32
+using System;
+
+public class ReverseString
+{
+    static void Main()
+    {
+        string txt = Console.ReadLine();
+
+        string rev = string.Empty;
+
+        for (int i = txt.Length - 1; i >= 0; i--)
+        {
+            rev += txt[i];
+        }
+
+        Console.WriteLine(rev);
+    }
+}
