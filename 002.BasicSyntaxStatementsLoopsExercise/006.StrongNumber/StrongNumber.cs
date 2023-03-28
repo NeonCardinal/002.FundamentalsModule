89ad1e62f32e6fc81ff0972139fc79b8a82a70c2
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-28 20:48:20 +0300

    add

---
+++ 002.BasicSyntaxStatementsLoopsExercise/006.StrongNumber/StrongNumber.cs
@@ -0,0 +1,37 @@
+﻿//Basic syntax, conditional statements and loops - exercise, Task 006
+//25.03.2023, 11:06
+using System;
+
+public class StrongNumber
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+        int tempNumber = n;
+
+        while (tempNumber > 0)
+        {
+            int currNumber = tempNumber % 10;
+            tempNumber /= 10;
+            int currFact = 1;
+
+            for (int i = 1; i <= currNumber; i++)
+            {
+                currFact *= i;
+            }
+
+            sum += currFact;
+        }
+
+        if (n == sum)
+        {
+            Console.WriteLine("yes");
+        }
+        else
+        {
+            Console.WriteLine("no");
+        }
+    }
+}
