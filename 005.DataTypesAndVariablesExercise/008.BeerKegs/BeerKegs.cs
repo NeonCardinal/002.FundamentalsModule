6504094163376d46a96084b311b53e81185b1f88
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-31 01:37:30 +0300

    add

---
+++ 005.DataTypesAndVariablesExercise/008.BeerKegs/BeerKegs.cs
@@ -0,0 +1,32 @@
+﻿//Data types and variables - exercise, Task 008
+//26.03.2023, 19:52
+using System;
+
+public class BeerKegs
+{
+    static void Main()
+    {
+        int n;
+        double greater = double.MinValue;
+        string greaterModel = string.Empty;
+
+        n = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+        {
+            string model = Console.ReadLine();
+            double radius = double.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            double volume = Math.PI * radius * radius * height;
+
+            if (volume >= greater)
+            {
+                greater = volume;
+                greaterModel = model;
+            }
+        }
+
+        Console.WriteLine(greaterModel);
+    }
+}
