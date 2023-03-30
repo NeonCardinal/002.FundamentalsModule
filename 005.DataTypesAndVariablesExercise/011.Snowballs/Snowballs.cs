033aeda9b5a87425af575f336ca9677dae3b5d9d
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-31 01:38:21 +0300

    add

---
+++ 005.DataTypesAndVariablesExercise/011.Snowballs/Snowballs.cs
@@ -0,0 +1,35 @@
+﻿//Data types and variables - exercise, Task 011
+//26.03.2023, 20:06
+using System;
+using System.Numerics;
+
+public class Snowballs
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        BigInteger greater = int.MinValue;
+        int greaterSnow = 0;
+        int greaterTime = 0;
+        BigInteger greaterQuality = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            int snowballSnow = int.Parse(Console.ReadLine());
+            int snowballTime = int.Parse(Console.ReadLine());
+            int snowballQuality = int.Parse(Console.ReadLine());
+
+            BigInteger snowBallValue = BigInteger.Pow((snowballSnow / snowballTime), snowballQuality);
+
+            if (snowBallValue >= greater)
+            {
+                greater = snowBallValue;
+                greaterSnow = snowballSnow;
+                greaterTime = snowballTime;
+                greaterQuality = snowballQuality;
+            }
+        }
+
+        Console.WriteLine($"{greaterSnow} : {greaterTime} = {greater} ({greaterQuality})");
+    }
+}
