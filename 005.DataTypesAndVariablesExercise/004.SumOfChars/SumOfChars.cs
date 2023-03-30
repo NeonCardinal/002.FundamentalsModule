c8cbb1674c4321f28e9f0f6517a76081abfa77af
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-31 01:36:22 +0300

    add

---
+++ 005.DataTypesAndVariablesExercise/004.SumOfChars/SumOfChars.cs
@@ -0,0 +1,22 @@
+﻿//Data types and variables - exercise, Task 004
+//26.03.2023, 12:25
+using System;
+
+public class SumOfChars
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+
+        for(int i = 0; i < n; i ++)
+        {
+            char ch = char.Parse(Console.ReadLine());
+
+            sum += (int)ch;
+        }
+
+        Console.WriteLine($"The sum equals: {sum}");
+    }
+}
