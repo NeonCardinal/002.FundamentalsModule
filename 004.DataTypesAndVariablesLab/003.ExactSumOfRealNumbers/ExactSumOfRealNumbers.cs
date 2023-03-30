f21f0dcbfce087539b074221b31e1e4d6d53a093
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-30 22:04:54 +0300

    add

---
+++ 004.DataTypesAndVariablesLab/003.ExactSumOfRealNumbers/ExactSumOfRealNumbers.cs
@@ -0,0 +1,22 @@
+﻿//Data types and variables - lab, Task 003
+//26.03.2023, 00:10
+using System;
+
+public class ExactSumOfRealNumbers
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        decimal sum = 0.00M;
+
+        for(int i = 0; i < n; i++)
+        {
+            decimal number = decimal.Parse(Console.ReadLine());
+
+            sum += number;
+        }
+
+        Console.WriteLine(sum);
+    }
+}
