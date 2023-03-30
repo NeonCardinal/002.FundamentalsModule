668771ce2aa94db43e0f048c0b64bdbc8a979fb3
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-31 01:37:13 +0300

    add

---
+++ 005.DataTypesAndVariablesExercise/007.WaterOverflow/WaterOverflow.cs
@@ -0,0 +1,28 @@
+﻿//Data types and variables - exercise, Task 007
+//26.03.2023, 19:50
+using System;
+
+public class WaterOverflow
+{
+    static void Main()
+    {
+        int waterTankCapacity = 255;
+        int capacity = 0;
+        int n = Int32.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            int litters = Int32.Parse(Console.ReadLine());
+            capacity += litters;
+            if (capacity > waterTankCapacity)
+            {
+                Console.WriteLine("Insufficient capacity!");
+                capacity -= litters;
+            }
+            if (i == n - 1)
+            {
+                Console.WriteLine(capacity);
+            }
+        }
+    }
+}
