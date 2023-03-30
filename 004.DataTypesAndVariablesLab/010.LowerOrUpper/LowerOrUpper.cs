47b378199af745a5152d4b55644b70c2619bbadb
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-30 22:12:39 +0300

    add

---
+++ 004.DataTypesAndVariablesLab/010.LowerOrUpper/LowerOrUpper.cs
@@ -0,0 +1,20 @@
+﻿//Data types and variables - lab, Task 010
+//26.03.2023, 00:33
+using System;
+
+public class LowerOrUpper
+{
+    static void Main()
+    {
+        char ch = char.Parse(Console.ReadLine());
+
+        if(char.IsUpper(ch))
+        {
+            Console.WriteLine("upper-case");
+        }
+        else
+        {
+            Console.WriteLine("lower-case");
+        }
+    }
+}
