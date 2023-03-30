216b34f6fa20429fc2809162e8ea069139945d19
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-30 22:12:22 +0300

    add

---
+++ 004.DataTypesAndVariablesLab/009.CharsToString/CharsToString.cs
@@ -0,0 +1,18 @@
+﻿//Data types and variables - lab, Task 009
+//26.03.2023, 00:29
+using System;
+
+public class CharsToString
+{
+    static void Main()
+    {
+        char a = char.Parse(Console.ReadLine());
+        char b = char.Parse(Console.ReadLine());
+        char c = char.Parse(Console.ReadLine());
+
+        string str = "";
+        str += a + "" + b + "" + c;
+
+        Console.WriteLine(str);
+    }
+}
