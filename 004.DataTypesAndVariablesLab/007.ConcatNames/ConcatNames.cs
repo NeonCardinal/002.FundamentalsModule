2472d21adfabaee0e6c18fe5e293a39e03f0a64e
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-30 22:11:43 +0300

    add

---
+++ 004.DataTypesAndVariablesLab/007.ConcatNames/ConcatNames.cs
@@ -0,0 +1,17 @@
+﻿//Data types and variables - lab, Task 007
+//26.03.2023, 00:23
+using System;
+
+public class ConcatNames
+{
+    static void Main()
+    {
+        string firstName = Console.ReadLine();
+        string lastName = Console.ReadLine();
+        string delimeter = Console.ReadLine();
+
+        string str = firstName + delimeter + lastName;
+
+        Console.WriteLine(str);
+    }
+}
