91d13040a70b7094eaaee09c8fc1b8468876c05d
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-30 22:11:26 +0300

    add

---
+++ 004.DataTypesAndVariablesLab/006.ReversedChars/ReversedChars.cs
@@ -0,0 +1,23 @@
+﻿//Data types and variables - lab, Task 006
+//26.03.2023, 00:19
+using System;
+
+public class ReversedChars
+{
+    static void Main()
+    {
+        string txt = "";
+
+        for(int i = 0; i < 3; i++)
+        {
+            char symbol = char.Parse(Console.ReadLine());
+
+            txt += symbol;
+        }
+
+        for(int i = txt.Length - 1; i >= 0; i--)
+        {
+            Console.Write($"{txt[i]} ");
+        }
+    }
+}
