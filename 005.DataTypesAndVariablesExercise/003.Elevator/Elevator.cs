f6b06198f1dbcd80a84fc61e55751c98714eba17
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-31 01:35:54 +0300

    add

---
+++ 005.DataTypesAndVariablesExercise/003.Elevator/Elevator.cs
@@ -0,0 +1,16 @@
+﻿//Data types and variables - exercise, Task 003
+//26.03.2023, 12:22
+using System;
+
+public class Elevator
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int p = int.Parse(Console.ReadLine());
+
+        int courses = (int)Math.Ceiling((double)n / p);
+
+        Console.WriteLine(courses);
+    }
+}
