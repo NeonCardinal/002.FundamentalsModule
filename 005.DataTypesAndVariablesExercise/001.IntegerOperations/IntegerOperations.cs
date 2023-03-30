ff3759dd1d671114e402849775eac2882ecddbd1
NeonCardinal <nvnnaidenov@gmail.com>
2023-03-31 01:35:19 +0300

    add

---
+++ 005.DataTypesAndVariablesExercise/001.IntegerOperations/IntegerOperations.cs
@@ -0,0 +1,20 @@
+﻿//Data types and variables - exercise, Task 001
+//26.03.2023, 12:13
+using System;
+
+public class IntegerOperations
+{
+    static void Main()
+    {
+        int firstNumber = int.Parse(Console.ReadLine());
+        int secondNumber = int.Parse(Console.ReadLine());
+        int thirdNumber = int.Parse(Console.ReadLine());
+        int fourthNumber = int.Parse(Console.ReadLine());
+
+        int result = firstNumber + secondNumber;
+        result /= thirdNumber;
+        result *= fourthNumber;
+
+        Console.WriteLine(result);
+    }
+}
