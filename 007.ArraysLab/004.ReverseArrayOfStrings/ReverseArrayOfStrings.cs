fdfc2e76a3be078977998a79e550e99006ea5205
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-02 01:06:01 +0300

    add

---
+++ 007.ArraysLab/004.ReverseArrayOfStrings/ReverseArrayOfStrings.cs
@@ -0,0 +1,18 @@
+﻿//Arrays - lab, Task 004
+//28.03.2023, 23:09
+using System;
+
+public class ReverseArrayOfStrings
+{
+    static void Main()
+    {
+        string[] arr = Console.ReadLine().Split();
+
+        for(int i = arr.Length - 1; i >= 0; i--)
+        {
+            Console.Write($"{arr[i]} ");
+        }
+
+        Console.WriteLine();
+    }
+}
