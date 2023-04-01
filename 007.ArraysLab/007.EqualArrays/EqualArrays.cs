f22e106e44f6d727f9274dba6c29e834407f36c5
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-02 01:06:50 +0300

    add

---
+++ 007.ArraysLab/007.EqualArrays/EqualArrays.cs
@@ -0,0 +1,21 @@
+﻿//Arrays - lab, Task 007
+//28.03.2023, 23:18
+using System;
+using System.Linq;
+
+public class EqualArrays
+{
+    static void Main()
+    {
+        int[] first = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int[] second = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        for(int i = 0; i < first.Length; i++)
+        {
+            if(first[i] != second[i])
+            {
+                Console.WriteLine($"Arrays are not identical. Found difference at {i} index.");
+            }
+        }
+    }
+}
