50cdfee593686cf6cf2c4aeab07461cf4f54d484
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-02 01:05:44 +0300

    add

---
+++ 007.ArraysLab/003.RoundingNumbers/RoundingNumbers.cs
@@ -0,0 +1,21 @@
+﻿//Data types and variables - lab, Task 003
+//28.03.2023, 23:06
+using System;
+using System.Linq;
+
+public class RoundingNumbers
+{
+    static void Main()
+    {
+        double[] arr = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write($"{arr[i]} => ");
+
+            arr[i] = Math.Round(arr[i], MidpointRounding.AwayFromZero);
+
+            Console.WriteLine($"{arr[i]}");
+        }
+    }
+}
