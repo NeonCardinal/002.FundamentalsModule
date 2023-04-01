3280cadfa0d9651cfdc073494653997c5caa68bd
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-01 16:00:29 +0300

    add

---
+++ 006.DataTypesAndVariablesMoreExercise/004.RefactoringPrimeChecker/RefactoringPrimeChecker.cs
@@ -0,0 +1,34 @@
+﻿//Data types and variables - more exercise, Task 004
+//28.03.2023, 22:35
+using System;
+
+public class RefactoringPrimeChecker
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        for (int i = 2; i <= number; i++)
+        {
+            bool isPrime = true;
+            for (int j = 2; j < i; j++)
+            {
+                if (i % j == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime)
+            {
+                Console.WriteLine($"{i} -> true");
+            }
+            else
+            {
+                Console.WriteLine($"{i} -> false");
+            }
+
+        }
+    }
+}
