42d3af6680d46ed6436bf5a8f89ce2da336e3dd4
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-02 01:06:32 +0300

    add

---
+++ 007.ArraysLab/006.EvenAndOddSubtraction/EvenAndOddSubtraction.cs
@@ -0,0 +1,31 @@
+﻿//Arrays - lab, Task 006
+//28.03.2023, 23:15
+using System;
+using System.Linq;
+
+public class EvenAndOddSubtraction
+{
+    static void Main()
+    {
+        int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        int evenSum = 0;
+        int oddSum = 0;
+
+        for(int i = 0; i < arr.Length; i++)
+        {
+            int number = arr[i];
+
+            if(number % 2 == 0)
+            {
+                evenSum += number;
+            }
+            else
+            {
+                oddSum += number;
+            }
+        }
+
+        Console.WriteLine(evenSum - oddSum);
+    }
+}
