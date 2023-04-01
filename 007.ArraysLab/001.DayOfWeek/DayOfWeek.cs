2fb7a32090b5b00d0dba98afa67b83ebac5e0717
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-02 01:05:05 +0300

    add

---
+++ 007.ArraysLab/001.DayOfWeek/DayOfWeek.cs
@@ -0,0 +1,23 @@
+﻿//Arrays - lab, Task 001
+//28.03.2023, 22:58
+using System;
+
+public class DayOfWeek
+{
+    static void Main()
+    {
+        string[] days = { "Monday", "Tuesday", "Wednesday",
+            "Thursday", "Friday", "Saturday", "Sunday" };
+
+        int day = int.Parse(Console.ReadLine());
+
+        if(day < 1 || day > 7)
+        {
+            Console.WriteLine("Invalid day!");
+        }
+        else
+        {
+            Console.WriteLine($"{days[day - 1]}");
+        }
+    }
+}
