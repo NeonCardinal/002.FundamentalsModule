0ac31672c2b737d746f8d05c3a5ce5a5b62165e8
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-03 11:19:27 +0300

    add

---
+++ 008.ArraysExercise/005.TopIntegers/TopIntegers.cs
@@ -0,0 +1,36 @@
+﻿//Arrays - exercise, Task 005
+//02.04.2023, 15:51
+using System;
+using System.Linq;
+
+public class TopIntegers
+{
+    static void Main()
+    {
+        int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        string greater = "";
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            int currentElement = arr[i];
+            bool isGreater = true;
+
+            for (int j = i + 1; j < arr.Length; j++)
+            {
+                if (arr[j] >= currentElement)
+                {
+                    isGreater = false;
+                    break;
+                }
+            }
+
+            if (isGreater)
+            {
+                greater += currentElement + " ";
+            }
+        }
+
+        Console.WriteLine(greater);
+    }
+}
