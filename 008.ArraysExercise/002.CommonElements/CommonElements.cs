30df7633b93655f86add9f53d6ebc1dc5b271158
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-03 11:18:31 +0300

    add

---
+++ 008.ArraysExercise/002.CommonElements/CommonElements.cs
@@ -0,0 +1,26 @@
+﻿//Arrays - exercise, Task 002
+//01.04.2023, 23:08
+using System;
+
+public class CommonElements
+{
+    static void Main()
+    {
+        string[] first = Console.ReadLine().Split();
+        string[] second = Console.ReadLine().Split();
+        string third = "";
+
+        for (int i = 0; i < first.Length; i++)
+        {
+            for (int j = 0; j < second.Length; j++)
+            {
+                if (second[j] == first[i])
+                {
+                    third += second[j] + " ";
+                }
+            }
+        }
+
+        Console.WriteLine(third);
+    }
+}
