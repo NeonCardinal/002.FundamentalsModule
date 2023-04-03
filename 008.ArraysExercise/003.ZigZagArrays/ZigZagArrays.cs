93af912b465d2ed711967f165729df18fa36aed6
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-03 11:18:49 +0300

    add

---
+++ 008.ArraysExercise/003.ZigZagArrays/ZigZagArrays.cs
@@ -0,0 +1,34 @@
+﻿//Arrays - exercise, Task 003
+//01.04.2023, 23:13
+using System;
+using System.Linq;
+
+public class ZigZagArrays
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int[] first = new int[n];
+        int[] second = new int[n];
+
+        for(int i = 1; i <= n; i++)
+        {
+            int[] tempArr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            if(i % 2 != 0)
+            {
+                first[i - 1] = tempArr[0];
+                second[i - 1] = tempArr[1];
+            }
+            else
+            {
+                first[i - 1] = tempArr[1];
+                second[i - 1] = tempArr[0];
+            }
+        }
+
+        Console.WriteLine(String.Join(" ", first));
+        Console.WriteLine(String.Join(" ", second));
+    }
+}
