0ede5e051f6b31a92c06bacf2dfa3898e8b675bc
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-03 11:18:13 +0300

    add

---
+++ 008.ArraysExercise/001.Train/Train.cs
@@ -0,0 +1,23 @@
+﻿//Arrays - exercise, Task 001
+//01.04.2023, 23:05
+using System;
+
+public class Train
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+            sum += arr[i];
+        }
+
+        Console.WriteLine(String.Join(" ", arr));
+        Console.WriteLine(sum);
+    }
+}
