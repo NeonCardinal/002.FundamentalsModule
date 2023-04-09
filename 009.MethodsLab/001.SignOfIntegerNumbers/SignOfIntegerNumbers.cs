53ed2d7de66335f681f55ace05d7bb8cc87c73c5
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-09 08:51:04 +0300

    add

---
+++ 009.MethodsLab/001.SignOfIntegerNumbers/SignOfIntegerNumbers.cs
@@ -0,0 +1,32 @@
+﻿//Methods - lab, Task 001
+//08.04.2023, 12:34
+using System;
+
+public class SignOfIntegerNumbers
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        CheckSign(number);
+    }
+    
+    /// <summary>
+    /// Проверява за знака на числото
+    /// </summary>
+    static void CheckSign(int number)
+    {
+        if(number > 0)
+        {
+            Console.WriteLine($"The number {number} is positive.");
+        }
+        else if(number < 0)
+        {
+            Console.WriteLine($"The number {number} is negative.");
+        }
+        else if(number == 0)
+        {
+            Console.WriteLine($"The number {number} is zero.");
+        }
+    }
+}
