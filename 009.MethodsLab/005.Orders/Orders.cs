6eb11ad684bd07fcfe6dcc70dfa9b7cc3c8184be
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-09 08:52:28 +0300

    add

---
+++ 009.MethodsLab/005.Orders/Orders.cs
@@ -0,0 +1,48 @@
+﻿//Methods - lab, Task 005
+//08.04.2023, 17:20
+using System;
+
+public class Orders
+{
+    static void Main()
+    {
+        string product = Console.ReadLine();
+        int quantity = int.Parse(Console.ReadLine());
+
+        CalulateThePrice(product, quantity);
+    }
+
+    static void CalulateThePrice(string product, int quantity)
+    {
+        double price = 0.00;
+        switch(product)
+        {
+            case "coffee":
+                {
+                    price = quantity * 1.50;
+                    Console.WriteLine($"{price:F2}");
+                }
+                break;
+            case "water":
+                {
+                    price = quantity * 1.00;
+                    Console.WriteLine($"{price:F2}");
+                }
+                break;
+            case "coke":
+                {
+                    price = quantity * 1.40;
+                    Console.WriteLine($"{price:F2}");
+                }
+                break;
+            case "snacks":
+                {
+                    price = quantity * 2.00;
+                    Console.WriteLine($"{price:F2}");
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
