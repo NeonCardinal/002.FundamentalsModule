bdd7e56f6c7e3beb540a9a209a3bb0bec4d748e9
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-09 08:52:08 +0300

    add

---
+++ 009.MethodsLab/004.PrintingTriangle/PrintingTriangle.cs
@@ -0,0 +1,36 @@
+﻿//Methods - lab, Task 004
+//08.04.2023, 17:14
+using System;
+
+public class PrintingTriangle
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        PrintTriangle(n);
+    }
+
+    static void PrintLine(int start, int end)
+    {
+        for(int i = start; i <= end; i++)
+        {
+            Console.Write(i + " ");
+        }
+
+        Console.WriteLine();
+    }
+
+    static void PrintTriangle(int n)
+    {
+        for(int i = 1; i <= n; i++)
+        {
+            PrintLine(1, i);
+        }
+
+        for(int i = n - 1; i >= 1; i--)
+        {
+            PrintLine(1, i);
+        }
+    }
+}
