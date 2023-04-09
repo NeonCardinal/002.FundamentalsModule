ee09e9ce45f6f91b2896e20aa5c5e982d50a20ee
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-09 08:52:47 +0300

    add

---
+++ 009.MethodsLab/006.CalculateRectangleArea/CalculateRectangleArea.cs
@@ -0,0 +1,21 @@
+﻿//Methods - lab, Task 006
+//08.04.2023, 18:29
+using System;
+
+public class CalculateRectangleArea
+{
+    static void Main()
+    {
+        double width = double.Parse(Console.ReadLine());
+        double height = double.Parse(Console.ReadLine());
+
+        double area = CalculateArea(width, height);
+
+        Console.WriteLine(area);
+    }
+
+    static double CalculateArea(double width, double height)
+    {
+        return width * height;
+    }
+}
