b4caeabf7c23e618f4455b17b2d6cf6b443a12b9
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-09 08:53:06 +0300

    add

---
+++ 009.MethodsLab/007.RepeatString/RepeatString.cs
@@ -0,0 +1,26 @@
+﻿//Methods - lab, Task 007
+//08.04.2023, 18:32
+using System;
+
+public class RepeatString
+{
+    static void Main()
+    {
+        string text = Console.ReadLine();
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(Repeat(text, n));
+    }
+
+    static string Repeat(string str, int times)
+    {
+        string result = string.Empty;
+
+        for(int i = 0; i < times; i++)
+        {
+            result += str;
+        }
+
+        return result;
+    }
+}
