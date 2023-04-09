27d721c816ea453885a2bac42fe6fd3cb21af6ac
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-09 08:53:25 +0300

    add

---
+++ 009.MethodsLab/008.MathPower/MathPower.cs
@@ -0,0 +1,26 @@
+﻿//Methods - lab, Task 008
+//08.04.2023, 18:35
+using System;
+
+public class MathPower
+{
+    static void Main()
+    {
+        double number = double.Parse(Console.ReadLine());
+        double power = double.Parse(Console.ReadLine());
+
+        Console.WriteLine(Power(number, power));
+    }
+
+    static double Power(double number, double power)
+    {
+        double result = 1;
+
+        for (int i = 1; i <= power; i++)
+        {
+            result *= number;
+        }
+
+        return result;
+    }
+}
