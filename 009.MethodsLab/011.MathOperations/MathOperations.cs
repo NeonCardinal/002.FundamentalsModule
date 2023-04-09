99902c41c4af0748b65a1763bb2684e7e7ac6b01
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-09 08:54:23 +0300

    add

---
+++ 009.MethodsLab/011.MathOperations/MathOperations.cs
@@ -0,0 +1,48 @@
+﻿//Methods - lab, Task 011
+//08.04.2023, 18:40
+using System;
+
+public class MathOperations
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        string opt = Console.ReadLine();
+        int b = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(Calculate(a, opt, b));
+    }
+
+    static double Calculate(int a, string opt, int b)
+    {
+        double result = 0.0;
+
+        switch (opt)
+        {
+            case "+":
+                {
+                    result = a + b;
+                }
+                break;
+            case "-":
+                {
+                    result = a - b;
+                }
+                break;
+            case "*":
+                {
+                    result = a * b;
+                }
+                break;
+            case "/":
+                {
+                    result = a / b;
+                }
+                break;
+            default:
+                break;
+        }
+
+        return result;
+    }
+}
