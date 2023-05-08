d7d39122f43e5c2fd8912b2e5f0dd316b84b0a62
nvnnaidenov <nvnnaidenov@gmail.com>
2023-05-08 21:02:39 +0300

    add

---
+++ 010.MethodsExercise/003.CharactersInRange/CharactersInRange.cs
@@ -0,0 +1,29 @@
+﻿//Methods - exercise, Task 003
+//08.05.2023, 16:24
+using System;
+
+public class CharactersInRange
+{
+    static void Main()
+    {
+        char first = char.Parse(Console.ReadLine());
+        char second = char.Parse(Console.ReadLine());
+
+        PrintChars(first, second);
+    }
+
+    static void PrintChars(char first, char second)
+    {
+        if(second < first)
+        {
+            char temp = first;
+            first = second;
+            second = temp;
+        }
+
+        for(char ch = (char)(first + 1); ch < second; ch++)
+        {
+            Console.Write($"{(char)ch} ");
+        }
+    }
+}
