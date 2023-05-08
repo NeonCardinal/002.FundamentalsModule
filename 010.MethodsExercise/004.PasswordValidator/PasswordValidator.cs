8d57612ceae1f77191993b4d5b943c70144d7168
nvnnaidenov <nvnnaidenov@gmail.com>
2023-05-08 21:02:57 +0300

    add

---
+++ 010.MethodsExercise/004.PasswordValidator/PasswordValidator.cs
@@ -0,0 +1,68 @@
+﻿//Methods - exercise, Task 004
+//08.05.2023, 16:34
+using System;
+
+public class PasswordValidator
+{
+    static void Main()
+    {
+        string pass = Console.ReadLine();
+
+        if (!CheckPassLen(pass))
+        {
+            Console.WriteLine("Password must be between 6 and 10 characters");
+        }
+        if (!CheckLetters(pass))
+        {
+            Console.WriteLine("Password must consist only of letters and digits");
+        }
+        if (!CheckForMinDigitsCount(pass))
+        {
+            Console.WriteLine("Password must have at least 2 digits");
+        }
+        if (CheckPassLen(pass) && CheckLetters(pass) && CheckForMinDigitsCount(pass))
+        {
+            Console.WriteLine("Password is valid");
+        }
+    }
+
+    static bool CheckPassLen(string str)
+    {
+        if (str.Length < 6 || str.Length > 10)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    static bool CheckLetters(string str)
+    {
+        foreach (char ch in str)
+        {
+            if (!Char.IsLetterOrDigit(ch))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static bool CheckForMinDigitsCount(string str)
+    {
+        int digitsCount = 0;
+
+        foreach (char ch in str)
+        {
+            if (Char.IsDigit(ch))
+            {
+                digitsCount++;
+            }
+        }
+
+        return digitsCount >= 2;
+    }
+}
