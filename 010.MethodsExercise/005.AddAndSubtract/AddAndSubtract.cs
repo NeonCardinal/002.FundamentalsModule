dc4c746cd5d1fa11392b64497ef558e0821c0230
nvnnaidenov <nvnnaidenov@gmail.com>
2023-05-08 21:03:15 +0300

    add

---
+++ 010.MethodsExercise/005.AddAndSubtract/AddAndSubtract.cs
@@ -0,0 +1,27 @@
+﻿//Methods - exercise, Task 005
+//08.05.2023, 16:39
+using System;
+
+public class AddAndSubtract
+{
+    static int Add(int firstNumber, int secondNumber)
+    {
+        return firstNumber + secondNumber;
+    }
+
+    static void Subtract(int firstNumber, int secondNumber)
+    {
+        Console.WriteLine(firstNumber - secondNumber);
+    }
+
+    static void Main()
+    {
+        int first = int.Parse(Console.ReadLine());
+        int second = int.Parse(Console.ReadLine());
+        int third = int.Parse(Console.ReadLine());
+
+        int sum = Add(first, second);
+
+        Subtract(sum, third);
+    }
+}
