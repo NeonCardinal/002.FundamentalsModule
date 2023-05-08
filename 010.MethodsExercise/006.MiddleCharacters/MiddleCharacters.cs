64f6fc82aa9acafb571daa8360f9fb9cb281f755
nvnnaidenov <nvnnaidenov@gmail.com>
2023-05-08 21:03:33 +0300

    add

---
+++ 010.MethodsExercise/006.MiddleCharacters/MiddleCharacters.cs
@@ -0,0 +1,25 @@
+﻿//Methods - exercise, Task 006
+//08.05.2023, 16:57
+using System;
+
+public class MiddleCharacters
+{
+    static void Main()
+    {
+        string text = Console.ReadLine();
+
+        Middle(text);
+    }
+
+    static void Middle(string text)
+    {
+        if(text.Length % 2 == 0)
+        {
+            Console.WriteLine(text[text.Length / 2 - 1] + "" + text[text.Length / 2]);
+        }
+        else
+        {
+            Console.WriteLine(text[text.Length / 2]);
+        }
+    }
+}
