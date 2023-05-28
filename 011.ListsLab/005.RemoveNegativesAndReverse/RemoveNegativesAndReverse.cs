0cefaed69a9f68045aa720fa1d1cfd34bacff234
nvnnaidenov <nvnnaidenov@gmail.com>
2023-05-28 16:43:30 +0300

    add

---
+++ 011.ListsLab/005.RemoveNegativesAndReverse/RemoveNegativesAndReverse.cs
@@ -0,0 +1,25 @@
+﻿//Lists - lab, Task 005
+//28.05.2023, 16:39
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class RemoveNegativesAndReverse
+{
+    static void Main()
+    {
+        List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+        numbers.RemoveAll(n => n < 0);
+
+        if(numbers.Count == 0)
+        {
+            Console.WriteLine("empty");
+        }
+        else
+        {
+            numbers.Reverse();
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
