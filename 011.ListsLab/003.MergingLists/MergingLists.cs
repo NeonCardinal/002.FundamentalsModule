077cce9db9d6d03e8ccd178c72e203357386cc9d
nvnnaidenov <nvnnaidenov@gmail.com>
2023-05-28 16:32:18 +0300

    add

---
+++ 011.ListsLab/003.MergingLists/MergingLists.cs
@@ -0,0 +1,31 @@
+﻿//Lists - lab, Task 003
+//28.05.2023, 16:20
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class MergingLists
+{
+    static void Main()
+    {
+        List<int> firstSerie = Console.ReadLine().Split().Select(int.Parse).ToList();
+        List<int> secondSerie = Console.ReadLine().Split().Select(int.Parse).ToList();
+        List<int> result = new List<int>();
+
+        int max = Math.Max(firstSerie.Count, secondSerie.Count);
+
+        for (int i = 0; i < max; i++)
+        {
+            if (i < firstSerie.Count)
+            {
+                result.Add(firstSerie[i]);
+            }
+            if (i < secondSerie.Count)
+            {
+                result.Add(secondSerie[i]);
+            }
+        }
+
+        Console.WriteLine(String.Join(" ", result));
+    }
+}
