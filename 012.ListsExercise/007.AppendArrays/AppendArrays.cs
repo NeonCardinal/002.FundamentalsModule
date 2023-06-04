b9ac549adb201c445d325df1a35b2a10fe1f1fdb
nvnnaidenov <nvnnaidenov@gmail.com>
2023-06-04 18:04:24 +0300

    add

---
+++ 012.ListsExercise/007.AppendArrays/AppendArrays.cs
@@ -0,0 +1,43 @@
+﻿//Lists - exercise, Task 007
+//03.06.2023, 18:46
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class AppendArrays
+{
+    static void Main()
+    {
+        //string[] text = Console.ReadLine().Split('|');
+        //string str = string.Empty;
+        //List<string> result = new List<string>();
+        //Array.Reverse(text);
+
+        //for(int i = 0; i < text.Length; i++)
+        //{
+        //    str += text[i];
+        //}
+
+        //result = str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+        //Console.WriteLine(string.Join(" ", result));
+
+        List<string> items = Console.ReadLine().Split('|').ToList();
+
+        items.Reverse();
+
+        List<string> result = new List<string>();
+
+        for (int i = 0; i < items.Count; i++)
+        {
+            List<string> numbers = items[i].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            for (int j = 0; j < numbers.Count; j++)
+            {
+                result.Add(numbers[j]);
+            }
+        }
+
+        Console.WriteLine(string.Join(" ", result));
+    }
+}
