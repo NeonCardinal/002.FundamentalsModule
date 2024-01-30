3ce5b94ec869a2e290e369f18f730b941e22114c
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-31 00:10:05 +0200

    add

---
+++ 001.BasicSyntaxConditionalStatLoopsLab/011.MultiplicationTableVTwo/MultiplicationTableVTwo.cs
@@ -0,0 +1,17 @@
+﻿//Basic syntax, con statements and loops, Task 011
+//26.01.2024, 19:15
+
+int number = int.Parse(Console.ReadLine());
+int multiplier = int.Parse(Console.ReadLine());
+
+if (multiplier > 10)
+{
+    Console.WriteLine($"{number} X {multiplier} = {number * multiplier}");
+}
+else
+{
+    for (int i = multiplier; i <= 10; i++)
+    {
+        Console.WriteLine($"{number} X {i} = {number * i}");
+    }
+}
