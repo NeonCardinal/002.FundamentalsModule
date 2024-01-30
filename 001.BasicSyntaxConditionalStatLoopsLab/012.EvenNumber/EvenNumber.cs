a1eaeb97549027b6e98eebd23e61fc9f6854e01b
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-31 00:10:23 +0200

    add

---
+++ 001.BasicSyntaxConditionalStatLoopsLab/012.EvenNumber/EvenNumber.cs
@@ -0,0 +1,17 @@
+﻿//Basic syntax, con statements and loops - lab, Task 012
+//26.01.2024, 19:19
+
+while(true)
+{
+    int number = int.Parse(Console.ReadLine());
+
+    if(number % 2 == 0)
+    {
+        Console.WriteLine($"The number is: {Math.Abs(number)}");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Please write an even number.");
+    }
+}
