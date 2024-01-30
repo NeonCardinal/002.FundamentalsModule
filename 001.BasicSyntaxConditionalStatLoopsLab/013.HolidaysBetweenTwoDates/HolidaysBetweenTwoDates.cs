fc2c3caa6e46dc6f8247556f4a111dd56280f494
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-31 00:10:44 +0200

    add

---
+++ 001.BasicSyntaxConditionalStatLoopsLab/013.HolidaysBetweenTwoDates/HolidaysBetweenTwoDates.cs
@@ -0,0 +1,19 @@
+﻿//Basic syntax, con statements and loops - lab, Task 013
+//26.01.2024, 19:22
+
+using System.Globalization;
+
+var startDate = DateTime.ParseExact(Console.ReadLine(), "d.M.yyyy", CultureInfo.InvariantCulture);
+var endDate = DateTime.ParseExact(Console.ReadLine(), "d.M.yyyy", CultureInfo.InvariantCulture);
+
+var holidaysCount = 0;
+
+for (var date = startDate; date <= endDate; date = date.AddDays(1))
+{
+    if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday)
+    {
+        holidaysCount++;
+    }
+}
+
+Console.WriteLine(holidaysCount);
