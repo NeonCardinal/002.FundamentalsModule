e22d19485d70eb3d8b7e233f2c1ab692fb1f2651
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-30 21:17:19 +0200

    add

---
+++ 001.BasicSyntaxConditionalStatLoopsLab/001.BasicSyntaxConditionalStatLoopsLab/StudentInformation.cs
@@ -0,0 +1,8 @@
+﻿//Basic syntax, con statements and loops - lab, Task 001
+//25.01.2024, 00:04
+
+string name = Console.ReadLine();
+int age = int.Parse(Console.ReadLine());
+double grade = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Name: {name}, Age: {age}, Grade: {grade:F2}");
