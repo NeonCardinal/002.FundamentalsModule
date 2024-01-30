fece7118e654a022def78080f71c7ad98e4195d6
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-30 21:17:53 +0200

    add

---
+++ 001.BasicSyntaxConditionalStatLoopsLab/003.PassedOrFailed/PassedOrFailed.cs
@@ -0,0 +1,13 @@
+﻿//Basic syntax, con statements and loops - lab, Task 003
+//25.01.2024, 00:08
+
+double grade = double.Parse(Console.ReadLine());
+
+if(grade >= 3.00)
+{
+    Console.WriteLine("Passed!");
+}
+else
+{
+    Console.WriteLine("Failed!");
+}
