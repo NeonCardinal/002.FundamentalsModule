88e00be37cba7dfdb7d74f96b2174bd2cbc0c064
NeonCardinal <nvnnaidenov@gmail.com>
2024-01-31 00:09:29 +0200

    add

---
+++ 001.BasicSyntaxConditionalStatLoopsLab/009.SumOfOddNumbers/SumOfOddNumbers.cs
@@ -0,0 +1,22 @@
+﻿//Basic syntax, con statements and loops, Task 009
+//26.01.2024, 18:56
+
+int n = int.Parse(Console.ReadLine());
+
+int sum = 0;
+int cnt = 0;
+
+for (int i = 1; i <= 100; i += 2)
+{
+    Console.WriteLine(i);
+
+    sum += i;
+    cnt++;
+
+    if (cnt == n)
+    {
+        break;
+    }
+}
+
+Console.WriteLine($"Sum: {sum}");
