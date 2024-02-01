b3e46863c9a6f4310b7460e6651117264305d70a
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-01 20:10:37 +0200

    add

---
+++ 002.BasicSyntaxConStatementsLoopsExercise/004.PrintAndSum/PrintAndSum.cs
@@ -0,0 +1,16 @@
+﻿//Basic syntax, con statements and loops - exercise, Task 004
+//27.01.2024, 12:15
+
+int start = int.Parse(Console.ReadLine());
+int end = int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+for(int i = start; i <= end; i++)
+{
+    Console.Write($"{i} ");
+    sum += i;
+}
+
+Console.WriteLine();
+Console.WriteLine($"Sum: {sum}");
