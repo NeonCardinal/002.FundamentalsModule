8daa4f90b481805930441e7724e6c7705d6dc27d
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-01 20:10:54 +0200

    add

---
+++ 002.BasicSyntaxConStatementsLoopsExercise/005.Login/Login.cs
@@ -0,0 +1,34 @@
+﻿//Basic syntax, con statements and loops - exercise, Task 005
+//27.01.2024, 12:18
+
+string username = Console.ReadLine();
+string pass = string.Empty;
+
+int cnt = 0;
+
+for (int i = username.Length - 1; i >= 0; i--)
+{
+    pass += username[i];
+}
+
+while (true)
+{
+    string password = Console.ReadLine();
+
+    if (password == pass)
+    {
+        Console.WriteLine($"User {username} logged in.");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Incorrect password. Try again.");
+        cnt++;
+    }
+
+    if (cnt >= 3)
+    {
+        Console.WriteLine($"User {username} blocked!");
+        break;
+    }
+}
