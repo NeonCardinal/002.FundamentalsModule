6582cdb5bb2b7f4dbc909f80530e148e2418e7ab
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-02 22:27:33 +0200

    add

---
+++ 002.BasicSyntaxConStatementsLoopsExercise/009.PadawanEquipment/PadawanEquipment.cs
@@ -0,0 +1,31 @@
+﻿//Basic syntax, con statements and loops - exercise, Task 009
+//30.01.2024, 21:35
+
+double johnsMoney = double.Parse(Console.ReadLine());
+int studentsCount = int.Parse(Console.ReadLine());
+double lightsaberPrice = double.Parse(Console.ReadLine());
+double robesPrice = double.Parse(Console.ReadLine());
+double beltPrice = double.Parse(Console.ReadLine());
+
+int extra = (int)Math.Ceiling(studentsCount * 0.10);
+int freeBelt = 0;
+
+for(int i = 1; i <= studentsCount; i++)
+{
+    if(i % 6 == 0)
+    {
+        freeBelt++;
+    }
+}
+
+double beltCost = beltPrice * (studentsCount - freeBelt);
+double total = lightsaberPrice * (studentsCount + extra) + robesPrice * studentsCount + beltCost;
+
+if(total <= johnsMoney)
+{
+    Console.WriteLine($"The money is enough - it would cost {total:F2}lv.");
+}
+else
+{
+    Console.WriteLine($"John will need {(total - johnsMoney):F2}lv more.");
+}
