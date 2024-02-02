c92fcf3de373f8b9954f69863c29fdcbb047f823
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-02 22:27:13 +0200

    add

---
+++ 002.BasicSyntaxConStatementsLoopsExercise/008.TriangleOfNumbers/TriangleOfNumbers.cs
@@ -0,0 +1,17 @@
+﻿//Basic syntax, con statements and loops - exercise, Task 008
+//30.01.2024, 21:31
+
+int n = int.Parse(Console.ReadLine());
+
+int number = 1;
+
+for(int i = 0; i < n; i++)
+{
+    for(int j = 0; j < number; j++)
+    {
+        Console.Write($"{number} ");
+    }
+
+    number++;
+    Console.WriteLine();
+}
