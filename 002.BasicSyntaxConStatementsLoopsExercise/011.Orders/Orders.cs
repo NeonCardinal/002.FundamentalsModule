886f2c52e6ab95a078a39af2ba72311a606995c7
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-02 22:28:08 +0200

    add

---
+++ 002.BasicSyntaxConStatementsLoopsExercise/011.Orders/Orders.cs
@@ -0,0 +1,20 @@
+﻿//Basic syntax, con statements and loops - exercise, Task 011
+//30.01.2024, 23:01
+
+int n = int.Parse(Console.ReadLine());
+
+double sum = 0.0;
+
+for(int i = 0; i < n; i++)
+{
+    double capsulePrice = double.Parse(Console.ReadLine());
+    int days = int.Parse(Console.ReadLine());
+    int capsuleCnt = int.Parse(Console.ReadLine());
+
+    double price = days * capsuleCnt * capsulePrice;
+    sum += price;
+
+    Console.WriteLine($"The price for the coffee is: ${price:F2}");
+}
+
+Console.WriteLine($"Total: ${sum:F2}");
