a896bab935a3c460a9c4c75829a82e3d82658e49
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-03 22:34:49 +0200

    add

---
+++ 003.BasicSyntaxConStatementsLoopsMoreExercise/001.SortNumbers/SortNumbers.cs
@@ -0,0 +1,16 @@
+﻿//Basic syntax, con statements and loops - more exercise, Task 001
+//30.01.2024, 23:17
+
+int firstNumber = int.Parse(Console.ReadLine());
+int secondNumber = int.Parse(Console.ReadLine());
+int thirdNumber = int.Parse(Console.ReadLine());
+
+int[] arr = new int[3];
+arr[0] = firstNumber;
+arr[1] = secondNumber;
+arr[2] = thirdNumber;
+
+Array.Sort(arr);
+Array.Reverse(arr);
+
+Console.WriteLine(string.Join("\r\n", arr));
