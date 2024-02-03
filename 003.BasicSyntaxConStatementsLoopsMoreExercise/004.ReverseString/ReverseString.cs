cdaaa71974bf19d79c63c50cdfe9b00a9495d1ae
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-03 22:35:53 +0200

    add

---
+++ 003.BasicSyntaxConStatementsLoopsMoreExercise/004.ReverseString/ReverseString.cs
@@ -0,0 +1,12 @@
+﻿//Basic syntax, con statements and loops - more exercise, Task 004
+//31.01.2024, 00:03
+
+string str = Console.ReadLine();
+string reversed = string.Empty;
+
+for(int i = str.Length - 1; i >= 0; i--)
+{
+    reversed += str[i];
+}
+
+Console.WriteLine(reversed);
