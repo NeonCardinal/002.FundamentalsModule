d0b996a8132b71e129fd4515d8de6682e150d21f
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-04 12:10:36 +0200

    add

---
+++ 004.DataTypesAndVariablesLab/005.SpecialNumbers/SpecialNumbers.cs
@@ -0,0 +1,26 @@
+﻿//Data types and variables - lab, Task 005
+//01.02.2024, 20:40
+
+int n = int.Parse(Console.ReadLine());
+
+
+for(int i = 1; i <= n; i++)
+{
+    int num = i;
+    int sum = 0;
+
+    while (num != 0)
+    {
+        sum += num % 10;
+        num = num / 10;
+    }
+
+    if (sum == 5 || sum == 7 || sum == 11)
+    {
+        Console.WriteLine($"{i} -> True");
+    }
+    else
+    {
+        Console.WriteLine($"{i} -> False");
+    }
+}
