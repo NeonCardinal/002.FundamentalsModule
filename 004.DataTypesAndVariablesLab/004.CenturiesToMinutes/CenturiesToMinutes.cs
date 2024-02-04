f1f5dd35e5f86ac357eaf72f3bf63e84aa969a53
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-04 12:10:18 +0200

    add

---
+++ 004.DataTypesAndVariablesLab/004.CenturiesToMinutes/CenturiesToMinutes.cs
@@ -0,0 +1,11 @@
+﻿//Data types and variables - lab, Task 004
+//01.02.2024, 20:38
+
+int centuries = int.Parse(Console.ReadLine());
+
+int years = centuries * 100;
+int days = (int)(years * 365.2422M);
+int hours = days * 24;
+int minutes = hours * 60;
+
+Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes");
