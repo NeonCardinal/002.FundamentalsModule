d0bdd09e1a61957f2dcd9430239c4567a490aa3a
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-04 12:09:19 +0200

    add

---
+++ 004.DataTypesAndVariablesLab/001.ConvertMetersToKilometers/ConvertMetersToKilometers.cs
@@ -0,0 +1,8 @@
+﻿//Data types and variables - lab, Task 001
+//01.02.2024, 20:20
+
+double meters = double.Parse(Console.ReadLine());
+
+double kilometers = meters / 1000.0;
+
+Console.WriteLine($"{kilometers:F2}");
