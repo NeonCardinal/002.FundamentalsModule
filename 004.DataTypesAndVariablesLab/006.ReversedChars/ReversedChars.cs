75f0f7cf67e1befc543ecb5fdabfd341f017f59d
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-04 12:10:56 +0200

    add

---
+++ 004.DataTypesAndVariablesLab/006.ReversedChars/ReversedChars.cs
@@ -0,0 +1,10 @@
+﻿//Data types and variables - lab, Task 006
+//01.02.2024, 20:55
+
+char first = char.Parse(Console.ReadLine());
+char second = char.Parse(Console.ReadLine());
+char third = char.Parse(Console.ReadLine());
+
+string str = third + " " + second + " " + first;
+
+Console.WriteLine(str);
