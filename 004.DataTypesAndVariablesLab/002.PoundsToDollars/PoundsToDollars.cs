8e8e083cd3a4d90edbf4f394cbb14c7ba5b179c2
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-04 12:09:40 +0200

    add

---
+++ 004.DataTypesAndVariablesLab/002.PoundsToDollars/PoundsToDollars.cs
@@ -0,0 +1,8 @@
+﻿//Data types and variables - lab, Task 002
+//01.02.2024, 20:24
+
+decimal pounds = decimal.Parse(Console.ReadLine());
+
+decimal dollars = pounds * 1.31m;
+
+Console.WriteLine($"{dollars:F3}");
