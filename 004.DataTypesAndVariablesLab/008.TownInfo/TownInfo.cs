96ba3c8b902f75e2f605a66182a1ab8615c7efa2
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-05 19:29:39 +0200

    add

---
+++ 004.DataTypesAndVariablesLab/008.TownInfo/TownInfo.cs
@@ -0,0 +1,8 @@
+﻿//Data types and variables - lab, Task 008
+//04.02.2024, 11:37
+
+string town = Console.ReadLine();
+long population = long.Parse(Console.ReadLine());
+int area = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Town {town} has population of {population} and area {area} square km.");
