71c597226dcf47e44b5a3b52fb15d3caeb85f7d5
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-05 19:29:56 +0200

    add

---
+++ 004.DataTypesAndVariablesLab/009.CharsToString/CharsToString.cs
@@ -0,0 +1,10 @@
+﻿//Data types and variables - lab, Task 009
+//04.02.2024, 11:54
+
+char a = char.Parse(Console.ReadLine());
+char b = char.Parse(Console.ReadLine());
+char c = char.Parse(Console.ReadLine());
+
+string str = "" + a + b + c;
+
+Console.WriteLine(str);
