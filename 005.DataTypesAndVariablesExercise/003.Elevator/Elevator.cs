ea2b204b1bd7d552bf32d4f558c17dc77cd05e24
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-07 15:47:39 +0200

    add

---
+++ 005.DataTypesAndVariablesExercise/003.Elevator/Elevator.cs
@@ -0,0 +1,9 @@
+﻿//Data types and variables - exercise, Task 003
+//04.02.2024, 13:56
+
+int people = int.Parse(Console.ReadLine());
+int capacity = int.Parse(Console.ReadLine());
+
+int courses = (int)Math.Ceiling((double)people / capacity);
+
+Console.WriteLine(courses);
