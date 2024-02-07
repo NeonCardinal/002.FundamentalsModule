9b8007e4a4813752cf5986ca955e42266cc182b7
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-07 15:48:11 +0200

    add

---
+++ 005.DataTypesAndVariablesExercise/005.PrintPartOfASCIITable/PrintPartOfASCIITable.cs
@@ -0,0 +1,10 @@
+﻿//Data types and variables - exercise, Task 005
+//04.02.2024, 14:03
+
+int start = int.Parse(Console.ReadLine());
+int end = int.Parse(Console.ReadLine());
+
+for(int i = start; i <= end; i++)
+{
+    Console.Write($"{(char)i} ");
+}
