8b02158e5c4da842336e91ea9d09e71eb19c1aed
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-07 15:47:00 +0200

    add

---
+++ 005.DataTypesAndVariablesExercise/001.IntegerOperations/IntegerOperations.cs
@@ -0,0 +1,11 @@
+﻿//Data types and variables - exercise, Task 001
+//04.02.2024, 12:16
+
+int firstNumber = int.Parse(Console.ReadLine());
+int secondNumber = int.Parse(Console.ReadLine());
+int thirdNumber = int.Parse(Console.ReadLine());
+int fourthNumber = int.Parse(Console.ReadLine());
+
+int sum = ((firstNumber + secondNumber) / thirdNumber) * fourthNumber;
+
+Console.WriteLine(sum);
