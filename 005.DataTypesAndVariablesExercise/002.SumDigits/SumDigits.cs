b39ab51427710b6b6e4c8b6b4baed214b1deda95
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-07 15:47:22 +0200

    add

---
+++ 005.DataTypesAndVariablesExercise/002.SumDigits/SumDigits.cs
@@ -0,0 +1,15 @@
+﻿//Data types and variables - exercise, Task 002
+//04.02.2024, 13:53
+
+int number = int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+while(number > 0)
+{
+    int n = number % 10;
+    sum += n;
+    number /= 10;
+}
+
+Console.WriteLine(sum);
