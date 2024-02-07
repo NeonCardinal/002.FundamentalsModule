b108004f36b1e54d13ed6b9371229ed042b8eb24
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-07 15:47:55 +0200

    add

---
+++ 005.DataTypesAndVariablesExercise/004.SumOfChars/SumOfChars.cs
@@ -0,0 +1,15 @@
+﻿//Data types and variables - exercise, Task 004
+//04.02.2024, 14:00
+
+int n = int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+for(int i = 0; i < n; i++)
+{
+    char ch = char.Parse(Console.ReadLine());
+
+    sum += (int)ch;
+}
+
+Console.WriteLine($"The sum equals: {sum}");
