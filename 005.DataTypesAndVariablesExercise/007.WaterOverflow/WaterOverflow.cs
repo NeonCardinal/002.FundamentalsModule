4987575064d7b26370adfd12729d26a46e5f910d
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-08 18:49:49 +0200

    add

---
+++ 005.DataTypesAndVariablesExercise/007.WaterOverflow/WaterOverflow.cs
@@ -0,0 +1,24 @@
+﻿//Data types and variables - exercise, Task 007
+//04.02.2024, 14:13
+
+int n = int.Parse(Console.ReadLine());
+
+int sum = 0;
+int capacity = 255;
+
+for(int i = 0; i < n; i++)
+{
+    int litters = int.Parse(Console.ReadLine());
+
+    if(litters > capacity)
+    {
+        Console.WriteLine("Insufficient capacity!");
+    }
+    else
+    {
+        capacity -= litters;
+        sum += litters;
+    }
+}
+
+Console.WriteLine(sum);
