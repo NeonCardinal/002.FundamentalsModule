0472386d8cdb778571de6778e3efd2a7a2264ab9
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-10 19:20:29 +0200

    add

---
+++ 006.DataTypesAndVariablesMoreExercise/003.FloatingEquality/FloatingEquality.cs
@@ -0,0 +1,20 @@
+﻿//Data types and variables - more exercise, Task 003
+//05.02.2024, 20:41
+
+const double Eps = 0.000001;
+double a = double.Parse(Console.ReadLine());
+double b = double.Parse(Console.ReadLine());
+double result = Math.Abs(a - b);
+
+if (result > Eps)
+{
+    Console.WriteLine("False");
+}
+else if (result < Eps)
+{
+    Console.WriteLine("True");
+}
+else if (result == Eps)
+{
+    Console.WriteLine("False");
+}
