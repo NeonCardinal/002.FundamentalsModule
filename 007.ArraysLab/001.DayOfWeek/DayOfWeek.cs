c61aff743025359a04578dbd52f60affe2f5bdf4
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-11 21:47:12 +0200

    add

---
+++ 007.ArraysLab/001.DayOfWeek/DayOfWeek.cs
@@ -0,0 +1,15 @@
+﻿//Arrays - lab, Task 001
+//07.02.2024, 18:34
+
+string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+int day = int.Parse(Console.ReadLine());
+
+if(day >= 1 && day <= days.Length)
+{
+    Console.WriteLine(days[day - 1]);
+}
+else
+{
+    Console.WriteLine("Invalid day!");
+}
