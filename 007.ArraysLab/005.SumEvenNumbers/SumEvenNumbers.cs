e55fd70d3a8c98eb7ec22961701901303f215d10
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-11 21:48:26 +0200

    add

---
+++ 007.ArraysLab/005.SumEvenNumbers/SumEvenNumbers.cs
@@ -0,0 +1,16 @@
+﻿//Arrays - lab, Task 005
+//07.02.2024, 19:03
+
+int sum = 0;
+int[] arr = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+for(int i = 0; i < arr.Length; i++)
+{
+    int curValue = arr[i];
+    if(curValue % 2 == 0)
+    {
+        sum += curValue;
+    }
+}
+
+Console.WriteLine(sum);
