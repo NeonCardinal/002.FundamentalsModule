c1bbf8fa1ea46a7e097ac0ae789eb22d842af83c
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-11 21:47:32 +0200

    add

---
+++ 007.ArraysLab/002.PrintNumbersInReverseOrder/PrintNumbersInReverseOrder.cs
@@ -0,0 +1,19 @@
+﻿//Arrays - lab, Task 002
+//07.02.2024, 18:41
+
+int n = int.Parse(Console.ReadLine());
+
+int[] arr = new int[n];
+
+for(int i = 0; i < arr.Length; i++)
+{
+    int number = int.Parse(Console.ReadLine());
+    arr[i] = number;
+}
+
+for(int i = arr.Length - 1; i >= 0; i--)
+{
+    Console.Write($"{arr[i]} ");
+}
+
+Console.WriteLine();
