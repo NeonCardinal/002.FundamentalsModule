29c91d85a16b131c0cc23d4ec5fbe655756be8af
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-11 21:47:49 +0200

    add

---
+++ 007.ArraysLab/003.RoundingNumbers/RoundingNumbers.cs
@@ -0,0 +1,13 @@
+﻿//Arrays - lab, Task 003
+//07.02.2024, 18:54
+
+double[] arr = Console.ReadLine().Split(" ").Select(double.Parse).ToArray();
+
+for(int i = 0; i < arr.Length; i++)
+{
+    Console.Write($"{arr[i]} => ");
+
+    arr[i] = Math.Round(arr[i], MidpointRounding.AwayFromZero);
+
+    Console.WriteLine($"{arr[i]}");
+}
