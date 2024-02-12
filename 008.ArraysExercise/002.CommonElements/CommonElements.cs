0353af06c815acff4dbbd84c5698ff76e3dded80
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-12 19:56:08 +0200

    add

---
+++ 008.ArraysExercise/002.CommonElements/CommonElements.cs
@@ -0,0 +1,19 @@
+﻿//Arrays - exercise, Task 002
+//08.02.2024, 19:45
+
+string[] first = Console.ReadLine().Split();
+string[] second = Console.ReadLine().Split();
+string third = "";
+
+for (int i = 0; i < first.Length; i++)
+{
+    for (int j = 0; j < second.Length; j++)
+    {
+        if (second[j] == first[i])
+        {
+            third += second[j] + " ";
+        }
+    }
+}
+
+Console.WriteLine(third);
