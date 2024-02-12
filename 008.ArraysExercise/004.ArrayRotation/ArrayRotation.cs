2afadc203e93a612361c5d4318af646c19748a53
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-12 19:56:42 +0200

    add

---
+++ 008.ArraysExercise/004.ArrayRotation/ArrayRotation.cs
@@ -0,0 +1,19 @@
+﻿//Arrays - exercise, Task 004
+//08.02.2024, 19:57
+
+int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int rot = int.Parse(Console.ReadLine());
+
+for(int i = 1; i <= rot; i++)
+{
+    int first = arr[0];
+
+    for(int j = 0; j <= arr.Length - 2; j++)
+    {
+        arr[j] = arr[j + 1];
+    }
+
+    arr[arr.Length - 1] = first;
+}
+
+Console.WriteLine(string.Join(" ", arr));
