2bf9e3d5114db28d7b3cd3a7ca82e7c86c7bafa7
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-13 22:26:44 +0200

    add

---
+++ 008.ArraysExercise/008.MagicSum/MagicSum.cs
@@ -0,0 +1,16 @@
+﻿//Arrays - exercise, Task 008
+//08.02.2024, 21:28
+
+int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < arr.Length; i++)
+{
+    for (int j = i + 1; j < arr.Length; j++)
+    {
+        if (arr[i] + arr[j] == n)
+        {
+            Console.WriteLine($"{arr[i]} {arr[j]}");
+        }
+    }
+}
