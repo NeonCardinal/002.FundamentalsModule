145e88543a9eb9a55d9982efd4f19f8acca8bcac
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-13 22:26:27 +0200

    add

---
+++ 008.ArraysExercise/007.MaxSequenceOfEqualElements/MaxSequenceOfEqualElements.cs
@@ -0,0 +1,34 @@
+﻿//Arrays - exercise, Task 007
+//08.02.2024, 21:21
+
+int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int start = 0;
+int len = 1;
+int bestStart = 0;
+int bestLen = 1;
+
+for (int i = 0; i < arr.Length - 1; i++)
+{
+    if (arr[i] != arr[i + 1])
+    {
+        start = i + 1;
+        len = 1;
+    }
+    else
+    {
+        len++;
+    }
+
+    if (len > bestLen)
+    {
+        bestLen = len;
+        bestStart = start;
+    }
+}
+
+for (int i = bestStart; i < bestStart + bestLen; i++)
+{
+    Console.Write($"{arr[i]} ");
+}
+
+Console.WriteLine();
