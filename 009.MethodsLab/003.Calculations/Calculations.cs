34c9ffdd4147e4d531eee4bc3fb7c464790e477e
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-14 23:39:02 +0200

    add

---
+++ 009.MethodsLab/003.Calculations/Calculations.cs
@@ -0,0 +1,43 @@
+﻿//Methods - lab, Task 003
+//11.02.2024, 21:52
+
+string cmd = Console.ReadLine().ToLower();
+int firstNumber = int.Parse(Console.ReadLine());
+int secondNumber = int.Parse(Console.ReadLine());
+
+if(cmd == "add")
+{
+    Add(firstNumber, secondNumber);
+}
+else if(cmd == "multiply")
+{
+    Multiply(firstNumber, secondNumber);
+}
+else if(cmd == "subtract")
+{
+    Subtract(firstNumber, secondNumber);
+}
+else if(cmd == "divide")
+{
+    Divide(firstNumber, secondNumber);
+}
+
+void Divide(int firstNumber, int secondNumber)
+{
+    Console.WriteLine(firstNumber / secondNumber);
+}
+
+void Subtract(int firstNumber, int secondNumber)
+{
+    Console.WriteLine(firstNumber - secondNumber);
+}
+
+void Multiply(int firstNumber, int secondNumber)
+{
+    Console.WriteLine(firstNumber * secondNumber);
+}
+
+void Add(int firstNumber, int secondNumber)
+{
+    Console.WriteLine(firstNumber + secondNumber);
+}
