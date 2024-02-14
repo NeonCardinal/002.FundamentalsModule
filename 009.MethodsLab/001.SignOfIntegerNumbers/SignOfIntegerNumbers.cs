22a84912eb757d2574e44cdfe0110333d7bdab56
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-14 23:38:18 +0200

    add

---
+++ 009.MethodsLab/001.SignOfIntegerNumbers/SignOfIntegerNumbers.cs
@@ -0,0 +1,22 @@
+﻿//Methods - lab, Task 001
+//10.02.2024, 19:41
+
+void SignOfNumbers(int number)
+{
+    if(number > 0)
+    {
+        Console.WriteLine($"The number {number} is positive.");
+    }
+    else if(number == 0)
+    {
+        Console.WriteLine($"The number {number} is zero.");
+    }
+    else if(number < 0)
+    {
+        Console.WriteLine($"The number {number} is negative.");
+    }
+}
+
+int number = int.Parse(Console.ReadLine());
+
+SignOfNumbers(number);
