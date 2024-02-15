1562fec0a52cf8a80bb65afad72671e9e0181187
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-16 00:34:52 +0200

    add

---
+++ 009.MethodsLab/006.CalculateRectangleArea/CalculateRectangleArea.cs
@@ -0,0 +1,14 @@
+﻿//Methods - lab, Task 006
+//12.02.2024, 21:49
+
+double a = double.Parse(Console.ReadLine());
+double b = double.Parse(Console.ReadLine());
+
+double area = GetRectangleArea(a, b);
+
+Console.WriteLine(area);
+
+double GetRectangleArea(double a, double b)
+{
+    return a * b;
+}
