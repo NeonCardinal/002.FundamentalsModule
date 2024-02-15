7be354ddf0beb59a4caccbcc69e63ebcef620c77
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-16 00:35:46 +0200

    add

---
+++ 009.MethodsLab/009.GreaterOfTwoValues/GreaterOfTwoValues.cs
@@ -0,0 +1,25 @@
+﻿//Methods - lab, Task 009
+//12.02.2024, 22:14
+
+string type = Console.ReadLine();
+
+int GetMax(int a, int b)
+{
+    return Math.Max(a, b);
+}
+
+char GetMax(char a, char b)
+{
+    char ch;
+
+    if(a > b)
+    {
+        ch = a;
+    }
+    else
+    {
+        ch = b;
+    }
+
+    return ch;
+}
