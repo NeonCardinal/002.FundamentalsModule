96ebd79643a2271f5d88d60fdf995a8c5ba7dd58
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-16 00:35:09 +0200

    add

---
+++ 009.MethodsLab/007.RepeatString/RepeatString.cs
@@ -0,0 +1,22 @@
+﻿//Methods - lab, Task 007
+//12.02.2024, 21:52
+
+string input = Console.ReadLine();
+int n = int.Parse(Console.ReadLine());
+
+string result = Repeat(input, n);
+
+Console.WriteLine(result);
+//Console.WriteLine(Repeat(input, n));
+
+string Repeat(string input, int n)
+{
+    string result = "";
+
+    for(int i = 0; i < n; i++)
+    {
+        result += input;
+    }
+
+    return result;
+}
