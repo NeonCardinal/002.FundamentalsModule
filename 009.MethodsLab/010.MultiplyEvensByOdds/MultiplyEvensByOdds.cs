69132762bc6fd4301a7a390ff62dc37ac501d1a9
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-16 00:36:03 +0200

    add

---
+++ 009.MethodsLab/010.MultiplyEvensByOdds/MultiplyEvensByOdds.cs
@@ -0,0 +1,51 @@
+﻿//Methods - lab, Task 010
+//12.02.2024, 23:03
+
+int n = int.Parse(Console.ReadLine());
+
+int result = GetMultipleOfEvenAndOdds(GetSumOfEvenDigits(n), GetSumOfOddDigits(n));
+
+Console.WriteLine(result);
+
+static int GetMultipleOfEvenAndOdds(int even, int odd)
+{
+    return even * odd;
+}
+
+static int GetSumOfEvenDigits(int number)
+{
+    int sum = 0;
+
+    while (number != 0)
+    {
+        int currNumber = number % 10;
+
+        if (currNumber % 2 == 0)
+        {
+            sum += number % 10;
+        }
+
+        number /= 10;
+    }
+
+    return sum;
+}
+
+static int GetSumOfOddDigits(int number)
+{
+    int sum = 0;
+
+    while (number != 0)
+    {
+        int currNumber = number % 10;
+
+        if (currNumber % 2 != 0)
+        {
+            sum += number % 10;
+        }
+
+        number /= 10;
+    }
+
+    return sum;
+}
