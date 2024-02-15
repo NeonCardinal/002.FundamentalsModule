da8c8a99614345604ce716fb189564701eea24b9
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-16 00:35:26 +0200

    add

---
+++ 009.MethodsLab/008.MathPower/MathPower.cs
@@ -0,0 +1,14 @@
+﻿//Methods - lab, Task 008
+//12.02.2024, 21:56
+
+double a = double.Parse(Console.ReadLine());
+double power = double.Parse(Console.ReadLine());
+
+Console.WriteLine(RisePower(a, power));
+
+double RisePower(double a, double power)
+{
+    double result = Math.Pow(a, power);
+
+    return result;
+}
