c0555d0211786bd6df1559dc036be714adafbe43
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-17 18:45:06 +0200

    update

--- 010.MethodsExercise/003.CharactersInRange/003.CharactersInRange.cs
+++ 010.MethodsExercise/003.CharactersInRange/003.CharactersInRange.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Methods - exercise, Task 003
+//17.02.2024, 17:42
+
+char start = char.Parse(Console.ReadLine());
+char end  = char.Parse(Console.ReadLine());
+
+if(end < start)
+{
+    char temp = start;
+    start = end; 
+    end = temp;
+}
+
+for(int i = start + 1; i < end; i++)
+{
+    Console.Write($"{(char)i} ");
+}
