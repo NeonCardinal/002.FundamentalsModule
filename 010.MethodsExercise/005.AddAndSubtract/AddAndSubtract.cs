a42ad082e4ab3bc701afa6c42bd7cec2ae72aedd
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-17 18:45:42 +0200

    add

---
+++ 010.MethodsExercise/005.AddAndSubtract/AddAndSubtract.cs
@@ -0,0 +1,20 @@
+﻿//Methods - exercise, Task 005
+//17.02.2024, 17:58
+
+int firstNumber = int.Parse(Console.ReadLine());
+int secondNumber = int.Parse(Console.ReadLine());
+int thirdNumber = int.Parse(Console.ReadLine());
+
+int first = Sum(firstNumber, secondNumber);
+
+Console.WriteLine(Subtract(first, thirdNumber));
+
+int Sum(int a, int b)
+{
+    return a + b;
+}
+
+int Subtract(int a, int b)
+{
+    return a - b;
+}
