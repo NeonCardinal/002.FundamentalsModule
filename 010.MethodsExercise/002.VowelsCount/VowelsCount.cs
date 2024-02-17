a06263776d31131c64b4a6cd4dfaa5d12c080cf0
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-17 17:40:23 +0200

    add

---
+++ 010.MethodsExercise/002.VowelsCount/VowelsCount.cs
@@ -0,0 +1,22 @@
+﻿//Methods - exercise, Task 002
+//13.02.2024, 23:18
+
+string input = Console.ReadLine();
+
+VowelCounter(input);
+
+void VowelCounter(string str)
+{
+    int cnt = 0;
+
+    for(int i = 0; i < str.Length; i++)
+    {
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'u' || str[i] == 'i' || str[i] == 'o'
+            || str[i] == 'A' || str[i] == 'E' || str[i] == 'U' || str[i] == 'I' || str[i] == 'O')
+        {
+            cnt++;
+        }
+    }
+
+    Console.WriteLine(cnt);
+}
