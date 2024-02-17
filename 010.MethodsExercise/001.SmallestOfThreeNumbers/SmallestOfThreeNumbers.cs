faad8f52280951fc789760240a685fd255e60b47
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-17 17:40:00 +0200

    add

---
+++ 010.MethodsExercise/001.SmallestOfThreeNumbers/SmallestOfThreeNumbers.cs
@@ -0,0 +1,28 @@
+﻿//Methods - lab, Task 001
+//13.02.2024, 22:33
+
+int first = int.Parse(Console.ReadLine());
+int second = int.Parse(Console.ReadLine());
+int third = int.Parse(Console.ReadLine());
+
+PrintSmallest(first, second, third);
+
+void PrintSmallest(int a, int b, int c)
+{
+    int smallest = int.MaxValue;
+
+    if(a < smallest)
+    {
+        smallest = a;
+    }
+    if(b < smallest)
+    {
+        smallest = b;
+    }
+    if(c < smallest)
+    {
+        smallest = c;
+    }
+
+    Console.WriteLine(smallest);
+}
