3c2dc8ea38e898b1e5459038ed6d897fb082eb31
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-18 14:04:09 +0200

    add

---
+++ 010.MethodsExercise/007.NxNMatrix/NxNMatrix.cs
@@ -0,0 +1,19 @@
+﻿//Methods - exercise, Task 007
+//17.02.2024, 18:23
+
+int n = int.Parse(Console.ReadLine());
+
+PrintMatrix(n);
+
+void PrintMatrix(int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            Console.Write($"{n} ");
+        }
+
+        Console.WriteLine();
+    }
+}
