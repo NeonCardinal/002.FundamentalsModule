1eb4acb8a176d5de1de88d0161a28c274be38a89
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-18 14:04:58 +0200

    add

---
+++ 010.MethodsExercise/009.PalindromeIntegers/PalindromeIntegers.cs
@@ -0,0 +1,35 @@
+﻿//Methods - exercise, Task 009
+//17.02.2024, 18:33
+
+while (true)
+{
+    string command = Console.ReadLine();
+
+    if (command == "END")
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine(IsPalidrome(command));
+    }
+}
+
+static bool IsPalidrome(string number)
+{
+    string reversed = "";
+
+    for (int i = number.Length - 1; i >= 0; i--)
+    {
+        reversed += number[i];
+    }
+
+    if (number == reversed)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
