eb26551e0556611a210dc4b0e88d8cb04b6346f7
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-18 14:04:29 +0200

    add

---
+++ 010.MethodsExercise/008.FactorialDivision/FactorialDivision.cs
@@ -0,0 +1,22 @@
+﻿//Methods - exercise, Task 008
+//17.02.2024, 18:27
+
+
+int firstNumber = int.Parse(Console.ReadLine());
+int secondNumber = int.Parse(Console.ReadLine());
+
+decimal result = Factoriel(firstNumber) / Factoriel(secondNumber);
+
+Console.WriteLine($"{result:F2}");
+
+decimal Factoriel(int number)
+{
+    decimal result = 1.0m;
+
+    for(int i = 1; i <= number; i++)
+    {
+        result *= i;
+    }
+
+    return result;
+}
