a8d5e04e13d20d6632a211c36405b3da74628bde
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-20 16:21:50 +0200

    add

---
+++ 011.ListLab/006.ListManipulationBasics/ListManipulationBasics.cs
@@ -0,0 +1,35 @@
+﻿//List - lab, Task 006
+//17.02.2024, 19:35
+
+List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+while(true)
+{
+    string cmd = Console.ReadLine().ToLower();
+
+    if(cmd == "end")
+    {
+        Console.WriteLine(string.Join(" ", numbers));
+        break;
+    }
+
+    string[] tokens = cmd.Split(" ").ToArray();
+    string command = tokens[0];
+
+    if(command == "add")
+    {
+        numbers.Add(int.Parse(tokens[1]));
+    }
+    else if(command == "remove")
+    {
+        numbers.Remove(int.Parse(tokens[1]));
+    }
+    else if(command == "removeat")
+    {
+        numbers.RemoveAt(int.Parse(tokens[1]));
+    }
+    else if(command == "insert")
+    {
+        numbers.Insert(int.Parse(tokens[2]), int.Parse(tokens[1]));
+    }
+}
