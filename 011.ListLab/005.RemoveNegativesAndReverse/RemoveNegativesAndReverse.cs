9f16656c1424f11457a6ba92e732844ae4ef0dab
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-20 16:21:34 +0200

    add

---
+++ 011.ListLab/005.RemoveNegativesAndReverse/RemoveNegativesAndReverse.cs
@@ -0,0 +1,16 @@
+﻿//List - lab, Task 005
+//17.02.2024, 19:26
+
+List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+numbers.RemoveAll(n => n < 0);
+numbers.Reverse();
+
+if (numbers.Count == 0)
+{
+    Console.WriteLine("empty");
+}
+else
+{
+    Console.WriteLine(String.Join(" ", numbers));
+}
