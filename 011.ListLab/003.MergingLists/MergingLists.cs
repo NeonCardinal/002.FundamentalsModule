de05a45523c990864a32b9c44c79967222e4b97d
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-20 16:21:01 +0200

    add

---
+++ 011.ListLab/003.MergingLists/MergingLists.cs
@@ -0,0 +1,22 @@
+﻿//List - lab, Task 003
+//17.02.2024, 19:14
+
+List<int> firstSerie = Console.ReadLine().Split().Select(int.Parse).ToList();
+List<int> secondSerie = Console.ReadLine().Split().Select(int.Parse).ToList();
+List<int> result = new List<int>();
+
+int max = Math.Max(firstSerie.Count, secondSerie.Count);
+
+for (int i = 0; i < max; i++)
+{
+    if (i < firstSerie.Count)
+    {
+        result.Add(firstSerie[i]);
+    }
+    if (i < secondSerie.Count)
+    {
+        result.Add(secondSerie[i]);
+    }
+}
+
+Console.WriteLine(String.Join(" ", result));
