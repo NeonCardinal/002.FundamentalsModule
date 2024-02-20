c59aa951360adb178fbb5275491a1100933620bc
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-20 16:20:43 +0200

    add

---
+++ 011.ListLab/002.GaussTrick/GaussTrick.cs
@@ -0,0 +1,17 @@
+﻿//List - lab, Task 002
+//17.02.2024, 19:03
+
+List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+List<int> result = new();
+
+for (int i = 0; i < numbers.Count / 2; i++)
+{
+    result.Add(numbers[i] + numbers[numbers.Count - 1 - i]);
+}
+
+if (numbers.Count % 2 != 0)
+{
+    result.Add(numbers[numbers.Count / 2]);
+}
+
+Console.WriteLine(String.Join(" ", result));
