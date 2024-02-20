6fb11d812928360e0cb48a8268979f7ecad56d50
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-20 16:21:16 +0200

    add

---
+++ 011.ListLab/004.ListOfProducts/ListOfProducts.cs
@@ -0,0 +1,21 @@
+﻿//List - lab, Task 004
+//17.02.2024, 19:20
+
+List<string> products = new List<string>();
+
+int n = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < n; i++)
+{
+    string product = Console.ReadLine();
+    products.Add(product);
+}
+
+products.Sort();
+
+for(int i = 0; i < products.Count; i++)
+{
+    int index = i + 1;
+
+    Console.WriteLine($"{index}.{products[i]}");
+}
