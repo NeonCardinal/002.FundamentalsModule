b855275e3830814a54f1dd7cd322f676e5637a03
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-20 16:20:26 +0200

    add

---
+++ 011.ListLab/001.SumAdjacentEqualNumbers/SumAdjacentEqualNumbers.cs
@@ -0,0 +1,16 @@
+﻿//List - lab, Task 001
+//17.02.2024, 18:54
+
+List<double> numbers = Console.ReadLine().Split(" ").Select(double.Parse).ToList();
+
+for(int i = 0; i < numbers.Count - 1; i++)
+{
+    if (numbers[i] == numbers[i + 1])
+    {
+        numbers[i] += numbers[i + 1];
+        numbers.RemoveAt(i + 1);
+        i = -1;
+    }
+}
+
+Console.WriteLine(string.Join(" ", numbers));
