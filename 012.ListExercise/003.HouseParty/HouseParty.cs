57aa20c689055909a1f7b37f1525da9b1c9956ae
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-22 00:13:07 +0200

    add

---
+++ 012.ListExercise/003.HouseParty/HouseParty.cs
@@ -0,0 +1,34 @@
+﻿//List - exercise, Task 003
+//21.02.2024, 21:54
+
+int cmdCount = int.Parse(Console.ReadLine());
+List<string> guests = new List<string>();
+
+for(int i = 0; i < cmdCount; i++)
+{
+    string[] tokens = Console.ReadLine().Split(" ").ToArray();
+    string name = tokens[0];
+
+    if(tokens.Length == 3)
+    {
+        if(guests.Contains(name))
+        {
+            Console.WriteLine($"{name} is already in the list!");
+            continue;
+        }
+
+        guests.Add(name);
+    }
+    else if(tokens.Length == 4)
+    {
+        if(!guests.Contains(name))
+        {
+            Console.WriteLine($"{name} is not in the list!");
+            continue;
+        }
+
+        guests.Remove(name);
+    }
+}
+
+Console.WriteLine(string.Join(System.Environment.NewLine, guests));
