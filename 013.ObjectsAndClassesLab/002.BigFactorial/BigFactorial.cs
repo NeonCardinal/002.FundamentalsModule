7b4b4d3d36d5e30ffa54d4026a19dd5f11d59001
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-24 18:37:42 +0200

    add

---
+++ 013.ObjectsAndClassesLab/002.BigFactorial/BigFactorial.cs
@@ -0,0 +1,14 @@
+﻿//Objects and classes - lab, Task 002
+//22.02.2024, 21:57
+
+using System.Numerics;
+
+int n = int.Parse(Console.ReadLine());
+BigInteger factoriel = 1;
+
+for(int i = 1; i <= n; i++)
+{
+    factoriel *= i;
+}
+
+Console.WriteLine(factoriel);
