726567909b8addda6b04533826acd5a20192a619
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-24 18:37:22 +0200

    add

---
+++ 013.ObjectsAndClassesLab/001.RandomizeWords/RandomizeWords.cs
@@ -0,0 +1,21 @@
+﻿//Objects and classes - lab, Task 001
+//21.02.2024, 22:39
+
+using System;
+
+string[] words = Console.ReadLine().Split(" ").ToArray();
+
+Random rnd = new Random();
+
+for (int i = 0; i < words.Length; i++)
+{
+    int rndIndex = rnd.Next(0, words.Length);
+    string temp = words[i];
+    words[i] = words[rndIndex];
+    words[rndIndex] = temp;
+}
+
+foreach (var item in words)
+{
+    Console.WriteLine(item);
+}
