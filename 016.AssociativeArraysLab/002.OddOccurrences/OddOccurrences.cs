84ee6d950c2582d5a60d022510f124e0e2807b2e
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-29 00:25:03 +0200

    add

---
+++ 016.AssociativeArraysLab/002.OddOccurrences/OddOccurrences.cs
@@ -0,0 +1,28 @@
+﻿//Associative arrays - lab, Task 002
+//28.02.2024, 23:04
+
+string[] words = Console.ReadLine().Split(" ").ToArray();
+
+Dictionary<string, int> count = new Dictionary<string, int>();
+
+foreach(var word in words)
+{
+    string wordToLower = word.ToLower();
+
+    if(!count.ContainsKey(wordToLower))
+    {
+        count.Add(wordToLower, 1);
+    }
+    else
+    {
+        count[wordToLower]++;
+    }
+}
+
+foreach(var word in count)
+{
+    if(word.Value % 2 != 0)
+    {
+        Console.Write($"{word.Key} ");
+    }
+}
