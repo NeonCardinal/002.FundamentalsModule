c92e69c34b555be7d42941dcf36da34c4d29c008
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-28 18:04:45 +0200

    add

---
+++ 014.ObjectsAndClassesExercise/003.ArticlesVTwo/ArticlesVTwo.cs
@@ -0,0 +1,41 @@
+﻿//Objects and classes - exercise, Task 003
+//27.02.2024, 23:49
+
+List<Article> articlesList = new List<Article>();
+
+int n = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < n; i++)
+{
+    string[] tokens = Console.ReadLine().Split(", ").ToArray();
+
+    Article art = new Article();
+
+    art.Title = tokens[0];
+    art.Content = tokens[1];
+    art.Author = tokens[2];
+
+    articlesList.Add(art);
+}
+
+foreach(var item in articlesList)
+{
+    Console.WriteLine(item.ToString());
+}
+
+public class Article
+{
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public string Author { get; set; }
+
+    public Article()
+    {
+
+    }
+
+    public override string ToString()
+    {
+        return new string($"{Title} - {Content}: {Author}");
+    }
+}
