b3bb0be242e8ce366b6b85eaa96adb7335d5c83a
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-29 00:25:37 +0200

    add

---
+++ 016.AssociativeArraysLab/004.WordFilter/WordFilter.cs
@@ -0,0 +1,6 @@
+﻿//Associative arrays - lab, Task 004
+//28.02.2024, 23:19
+
+string[] words = Console.ReadLine().Split(" ").Where(x => x.Length % 2 == 0).ToArray();
+
+Console.WriteLine(string.Join("\r\n", words));
