01a3acc8dfa551fa2ac59bd823c80f83f9ba9e23
NeonCardinal <nvnnaidenov@gmail.com>
2024-02-29 00:24:45 +0200

    add

---
+++ 016.AssociativeArraysLab/001.CountRealNumbers/CountRealNumbers.cs
@@ -0,0 +1,23 @@
+﻿//Associative arrays - lab, Task 001
+//28.02.2024, 22:45
+
+int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+SortedDictionary<int, int> occur = new SortedDictionary<int, int>();
+
+foreach(var item in numbers)
+{
+    if(!occur.ContainsKey(item))
+    {
+        occur.Add(item, 1);
+    }
+    else
+    {
+        occur[item]++;
+    }
+}
+
+foreach(var item in occur)
+{
+    Console.WriteLine($"{item.Key} -> {item.Value}");
+}
