80ac72e911d16489acdb22287d3f9b2a95d4883d
NeonCardinal <nvnnaidenov@gmail.com>
2024-03-01 00:51:02 +0200

    add

---
+++ 017.AssociativeArraysExercise/001.CountCharsInAString/CountCharsInAString.cs
@@ -0,0 +1,26 @@
+﻿//Associative arrays - exercise, Task 001
+//28.02.2024, 23:34
+
+string str = Console.ReadLine();
+
+Dictionary<char, int> occur =  new Dictionary<char, int>();
+
+foreach(char ch in str)
+{
+    if(ch != ' ')
+    {
+        if(!occur.ContainsKey(ch))
+        {
+            occur.Add(ch, 1);
+        }
+        else
+        {
+            occur[ch]++;
+        }
+    }
+}
+
+foreach(var item in occur)
+{
+    Console.WriteLine($"{item.Key} -> {item.Value}");
+}
