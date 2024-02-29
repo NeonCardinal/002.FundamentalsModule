86410e6b8b263c05e174e9821a6b9f62a468322d
NeonCardinal <nvnnaidenov@gmail.com>
2024-03-01 00:51:23 +0200

    add

---
+++ 017.AssociativeArraysExercise/002.AMinerTask/AMinerTask.cs
@@ -0,0 +1,39 @@
+﻿//Associative arrays - exercise, Task 002
+//28.02.2024, 23:43
+
+int i = 1;
+string currectKey = string.Empty;
+Dictionary<string, int> resource  = new Dictionary<string, int>();
+
+while(true)
+{
+    string input = Console.ReadLine();
+
+    if(input == "stop")
+    {
+        break;
+    }
+
+    if(i % 2 != 0)
+    {
+        currectKey = input;
+    }
+    else
+    {
+        if(!resource.ContainsKey(currectKey))
+        {
+            resource.Add(currectKey, int.Parse(input));
+        }
+        else
+        {
+            resource[currectKey] += int.Parse(input);
+        }
+    }
+
+    i++;
+}
+
+foreach(var item in resource)
+{
+    Console.WriteLine($"{item.Key} -> {item.Value}");
+}
