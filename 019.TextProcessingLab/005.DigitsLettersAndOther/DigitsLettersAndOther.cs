02cfc22654aaf99a0f26d52f1e4c4a1bdc0674dd
NeonCardinal <nvnnaidenov@gmail.com>
2024-03-12 21:31:05 +0200

    add

---
+++ 019.TextProcessingLab/005.DigitsLettersAndOther/DigitsLettersAndOther.cs
@@ -0,0 +1,30 @@
+﻿//Text processing - lab, Task 005
+//10.03.2024, 16:52
+
+using System.Runtime.Remoting;
+
+string str = Console.ReadLine();
+
+string digits = string.Empty;
+string letters = string.Empty;
+string others = string.Empty;
+
+for(int i = 0; i < str.Length; i++)
+{
+    if(char.IsDigit(str[i]))
+    {
+        digits += str[i];
+    }
+    else if(char.IsLetter(str[i]))
+    {
+        letters += str[i];
+    }
+    else// if (char.IsSymbol(str[i]))
+    {
+        others += str[i];
+    }
+}
+
+Console.WriteLine(digits);
+Console.WriteLine(letters);
+Console.WriteLine(others);
