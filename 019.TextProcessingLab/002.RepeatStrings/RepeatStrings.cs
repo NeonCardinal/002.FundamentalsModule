cd340cbca23f2e85561fe17d48fae4e2675e40f1
NeonCardinal <nvnnaidenov@gmail.com>
2024-03-12 21:30:01 +0200

    add

---
+++ 019.TextProcessingLab/002.RepeatStrings/RepeatStrings.cs
@@ -0,0 +1,18 @@
+﻿//Text processing - lab, Task 002
+//10.03.2024, 16:22
+
+using System.Text;
+
+string[] input = Console.ReadLine().Split(" ").ToArray();
+
+StringBuilder sb = new StringBuilder();
+
+foreach(var word in input)
+{
+    for(int i = 0; i < word.Length; i++)
+    {
+        sb.Append(word);
+    }
+}
+
+Console.WriteLine(sb.ToString());
