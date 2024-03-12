a62de738b291db5e1992d0186429e06490cd856e
NeonCardinal <nvnnaidenov@gmail.com>
2024-03-12 21:29:35 +0200

    add

---
+++ 019.TextProcessingLab/001.ReverseStrings/ReverseStrings.cs
@@ -0,0 +1,22 @@
+﻿//Text processing - lab, Task 001
+//04.02.2024, 20:54
+
+while(true)
+{
+    string input = Console.ReadLine();
+
+    if(input == "end")
+    {
+        break;
+    }
+
+    string reversed = "";
+
+    for(int i = input.Length - 1; i >= 0; i--)
+    {
+
+        reversed += input[i];
+    }
+
+    Console.WriteLine($"{input} = {reversed}");
+}
