08567892b9e7ef7f3eac3ed9059b1085a2b352eb
NeonCardinal <nvnnaidenov@gmail.com>
2024-03-12 21:30:24 +0200

    add

---
+++ 019.TextProcessingLab/003.Substring/Substring.cs
@@ -0,0 +1,13 @@
+﻿//Text processing - lab, Task 003
+//10.03.2024, 16:29
+
+string word = Console.ReadLine();
+string str = Console.ReadLine();
+
+while(str.IndexOf(word) != -1)
+{
+    int index = str.IndexOf(word);
+    str = str.Remove(index, word.Length);
+}
+
+Console.WriteLine(str);
