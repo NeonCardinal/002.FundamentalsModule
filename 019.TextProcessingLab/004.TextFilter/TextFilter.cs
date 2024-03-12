bb7b330c81bbf68b1ad6b1c0204d460e1b7e4fd7
NeonCardinal <nvnnaidenov@gmail.com>
2024-03-12 21:30:46 +0200

    add

---
+++ 019.TextProcessingLab/004.TextFilter/TextFilter.cs
@@ -0,0 +1,12 @@
+﻿//Text processing - lab, Task 004
+//10.03.2024, 16:39
+
+string[] bannedWords = Console.ReadLine().Split(" ");
+string text = Console.ReadLine();
+
+foreach(var word in bannedWords)
+{
+    text = text.Replace(word, new string('*', word.Length));
+}
+
+Console.WriteLine(text);
