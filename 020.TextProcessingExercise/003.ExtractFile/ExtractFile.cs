d66d5da08725b113fead287084a5cf1a72defd68
NeonCardinal <nvnnaidenov@gmail.com>
2024-03-13 21:31:13 +0200

    add

---
+++ 020.TextProcessingExercise/003.ExtractFile/ExtractFile.cs
@@ -0,0 +1,13 @@
+﻿//Text processing - exercise, Task 003
+//11.03.2024, 22:18
+
+string path = Console.ReadLine();
+
+int backSlashIndex = path.LastIndexOf('\\');
+string fileInfo = path.Substring(backSlashIndex + 1);
+int dotIndex = fileInfo.LastIndexOf('.');
+string fileName = fileInfo.Substring(0, dotIndex);
+string extensionName = fileInfo.Substring(dotIndex + 1);
+
+Console.WriteLine($"File name: {fileName}");
+Console.WriteLine($"File extension: {extensionName}");
