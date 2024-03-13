c878366734e8cc6485ffee332cd073ed96675fd7
NeonCardinal <nvnnaidenov@gmail.com>
2024-03-13 21:32:17 +0200

    add

---
+++ 020.TextProcessingExercise/006.ReplaceRepeatingChars/ReplaceRepeatingChars.cs
@@ -0,0 +1,16 @@
+﻿//Text processing - exercise, Task 006
+//11.03.2024, 22:23
+
+var str = Console.ReadLine().ToList(); ;
+string result = string.Empty;
+
+for (int i = 0; i < str.Count - 1; i++)
+{
+    if (str[i] == str[i + 1])
+    {
+        str.RemoveAt(i);
+        i--;
+    }
+}
+
+Console.WriteLine(string.Join("", str));
