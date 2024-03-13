f30993a4345dfb9dfd6c80edf29e961c8caa44a7
NeonCardinal <nvnnaidenov@gmail.com>
2024-03-13 21:31:34 +0200

    add

---
+++ 020.TextProcessingExercise/004.CaesarCipher/CaesarCipher.cs
@@ -0,0 +1,14 @@
+﻿//Text processing - exercise, Task 004
+//11.03.2024, 22:20
+
+using System.Text;
+
+string str = Console.ReadLine();
+StringBuilder sb = new StringBuilder();
+
+for (int i = 0; i < str.Length; i++)
+{
+    sb.Append((char)((int)str[i] + 3));
+}
+
+Console.WriteLine(sb.ToString());
